5afc44e2c95521d5233c5f913c74a0c5d1105d03
Levell x Vellbear <70419875+vellbear@users.noreply.github.com>
2023-11-19 13:00:17 -0800

    added navbar component (#5)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { NextUIProvider } from "@nextui-org/react";
 import { Button } from "@nextui-org/react";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <NextUIProvider>
       <main className="text-center">
-        <h1 className="font-semibold text-7xl font-serif text-center mt-16">
+        <Navbar />
+        <h1 className="mt-16 font-serif font-semibold text-center text-7xl">
           temerity
         </h1>
         <Button color="primary"> Next Button Test </Button>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import LOGO from "../../public/Logo.png";
+import BACK_ICON from "../../public/Back.png";
+import MENU_ICON from "../../public/Menu.png";
+import SEARCH_ICON from "../../public/Search.png";
+
+interface NavbarProps {
+  back?: boolean;
+}
+
+export default function Navbar({ back }: NavbarProps) {
+  return (
+    <nav className="fixed left-0 right-0 z-50 flex items-center justify-between bg-white px-7">
+      <Image
+        width={32}
+        height={32}
+        src={back ? BACK_ICON : MENU_ICON}
+        alt={back ? "back" : "menu"}
+      />
+      <Image width={95} height={39} src={LOGO} alt="logo" />
+      <Image width={36} height={36} src={SEARCH_ICON} alt="search" />
+    </nav>
+  );
+}
