0c06732c7239bc60af2a81a580323e9be83bcb9f
Llxovell <70419875+vellbear@users.noreply.github.com>
2023-11-19 16:02:57 -0800

    removed next-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
 "use client";
-import { NextUIProvider } from "@nextui-org/react";
-import { Button } from "@nextui-org/react";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <NextUIProvider>
-      <main className="text-center">
-        <Navbar />
-        <h1 className="mt-16 font-serif font-semibold text-center text-7xl">
-          temerity
-        </h1>
-        <Button color="primary"> Next Button Test </Button>
-      </main>
-    </NextUIProvider>
+    <main className="text-center">
+      <Navbar></Navbar>
+    </main>
   );
 }
